7e5e13df3f735544a2ef301a146b2a9b145f53f2
thadmuchnok64 <58534647+thadmuchnok64@users.noreply.github.com>
2024-01-17 12:45:31 -0500

    Unreal messages work now!

--- Server/TestServer/ServerHandle.cs
+++ Server/TestServer/ServerHandle.cs
@@ -15,10 +15,17 @@ namespace TestServer
 
 			Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}!\n" +
 				$"this user is using {_username}");
-			if(_fromClient != _clientIdCheck)
+			if (_fromClient != _clientIdCheck)
 			{
 				Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
 			}
+		}
+
+		public static void TestRecieved(int _fromClient, Packet _packet)
+		{
+			int _clientIdCheck = _packet.ReadInt();
+			string _message = _packet.ReadString();
+			Console.WriteLine($" Player {Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} says {_message}");
 		}
 	}
 }
