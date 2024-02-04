7a974200ce28c452090dcc3239028c0bb3e1ea6b
thejjm <89481210+thejjm@users.noreply.github.com>
2024-02-03 20:57:29 -0500

    Unreal now parses strings with a switch
    Parses messages and can start certain tasks easily

---
+++ Unity/Outpost Horizon Mobile/Assets/Scripts/JScripts/RodBehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class RodBehaviour : MonoBehaviour
+{
+    public static RodBehaviour instance;
+    public TMP_Text text;
+    public static List<int> rods = new List<int>();
+    string rodstoinsert;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null)
+        {
+            Debug.Log("Multiple Rod Behaviour!");
+            Destroy(this);
+        }
+        instance = this;
+    }
+
+    // Update is called once per frame
+    public void RodsTUpdate()
+    {
+        for (int i = 0; i < rods.Count; i++)
+        {
+            rodstoinsert = rodstoinsert + " ," + rods[i].ToString();
+        }
+        text.text = "Fuel Rods are Active. Please Insert Rod(s):" + rodstoinsert;
+    }
+}
