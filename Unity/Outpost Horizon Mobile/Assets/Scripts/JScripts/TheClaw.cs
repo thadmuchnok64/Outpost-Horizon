e2b8190b3fce3a875130b7885ad0e53a0c15f2de
thejjm <89481210+thejjm@users.noreply.github.com>
2024-02-20 11:53:40 -0500

    Finished the claw in Unity
    Need to still connect the claw and unreal

--- Unity/Outpost Horizon Mobile/Assets/Scripts/JScripts/TheClaw.cs
+++ Unity/Outpost Horizon Mobile/Assets/Scripts/JScripts/TheClaw.cs
@@ -16,9 +16,11 @@ public class TheClaw : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (claw.transform.localPosition.y < 3.59f && claw.transform.localPosition.y > 1)
+        if (claw.transform.localPosition.y <= 3.59f && claw.transform.localPosition.y >= .33f)
             claw.transform.Translate(stick.transform.localPosition * Time.deltaTime);
-        else if (claw.transform.localPosition.y > 3.58f)
-
+        else if (claw.transform.localPosition.y > 3.59f)
+            claw.transform.localPosition = new Vector3(0, 3.59f, 0);
+        else if (claw.transform.localPosition.y < .33f)
+            claw.transform.localPosition = new Vector3(0, .33f, 0);
     }
 }
