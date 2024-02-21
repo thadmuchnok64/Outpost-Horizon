fe5df2484ad98a4b7c684a21d75d54431f407c44
thejjm <89481210+thejjm@users.noreply.github.com>
2024-02-21 11:03:27 -0500

    Chat
    Chat has been implemented, albeit without GPT.

---
+++ Unity/Outpost Horizon Mobile/Assets/Scripts/JScripts/ChatSendMessage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatSendMessage : MonoBehaviour
+{
+    public RectTransform content;
+    public TMP_InputField input;
+    public TMP_Text output;
+    string text;
+    Vector2 newSize;
+    public void SendToChat()
+    {
+        text = input.text;
+        input.text = "";
+        output.text = output.text + "\n"+ "You:" + text;
+        output.text = output.text + "\n Helpy: The servers are currently undergoing maintenance, please try again later.";
+        if (output.text.Length > 200)
+        {
+            newSize = content.sizeDelta + new Vector2(0,100);
+            content.sizeDelta = newSize;
+            output.gameObject.transform.localPosition += new Vector3 (0,50,0);
+        }
+    }
+}
