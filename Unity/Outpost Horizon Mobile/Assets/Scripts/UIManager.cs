c48ce5b14b20369fc88ea6d6a9fb7a793b715e0b
thejjm <89481210+thejjm@users.noreply.github.com>
2024-03-24 19:39:34 -0400

    Added Sparks and an IP port method to Unity
    This makes it easier to connect

--- Unity/Outpost Horizon Mobile/Assets/Scripts/UIManager.cs
+++ Unity/Outpost Horizon Mobile/Assets/Scripts/UIManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
 
     public GameObject startMenu;
+    public TMP_InputField IPcode;
     public static bool connected = false;
 
 	private void Awake()
@@ -23,6 +26,13 @@ public class UIManager : MonoBehaviour
 
 	public void ConnectToServer()
 	{
+        if (IPcode.text != "")
+        {
+            string[] separ = IPcode.text.Split(':', 2);
+            TestClient.instance.ip = separ[0];
+            TestClient.instance.port = Int32.Parse(separ[1]);
+        }
+        IPcode.gameObject.SetActive(false);
 		startMenu.SetActive(false);
 		//usernameField.interactable= false;
 		TestClient.instance.ConnectToServer();
