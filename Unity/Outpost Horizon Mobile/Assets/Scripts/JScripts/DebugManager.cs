fd3e08d31da73d4ff21496057c11f316859e23d9
thejjm <89481210+thejjm@users.noreply.github.com>
2024-04-01 14:55:10 -0400

    Debug and Server Changes
    if any functionality of the app needs testing please let me know

--- Unity/Outpost Horizon Mobile/Assets/Scripts/JScripts/DebugManager.cs
+++ Unity/Outpost Horizon Mobile/Assets/Scripts/JScripts/DebugManager.cs
@@ -5,6 +5,9 @@ using TMPro;
 using UnityEngine;
 
 public class DebugManager : MonoBehaviour
+    /* IMPORTANT: The debug manager must be updated regularly with other scripts.
+    Its main function is to allow testing without the use of VR, to speed up
+    development. */
 {
     public static DebugManager instance;
     public GameObject debugPage;
@@ -33,5 +36,24 @@ public class DebugManager : MonoBehaviour
             debugPage.SetActive(true);
         else if (debugPage.activeSelf == true)
             debugPage.SetActive(false);
+    }
+    public void OnDebugSend()
+    {
+        if (ClientSendMessage._csmsg[0] == '/')
+        {
+            ClientSendMessage._csmsg = ClientSendMessage._csmsg.Replace("/","");
+            //UPDATE this with ClientHandle.ReadMessage
+            switch(ClientSendMessage._csmsg)
+            {
+                case "RodOnFloor!":
+                    RodBehaviour.instance.ShowError();
+                    break;
+                case "CraneError!":
+                    TheClaw.instance.CraneError();
+                    break;
+            }
+        }
+        else
+            UIManager.instance.SendToServer(ClientSendMessage._csmsg);
     }
 }
