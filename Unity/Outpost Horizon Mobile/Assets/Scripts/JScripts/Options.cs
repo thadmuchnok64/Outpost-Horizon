8c9fae6e7ed2501ee01e2dc8fba18dcda41d2018
thejjm <89481210+thejjm@users.noreply.github.com>
2024-04-21 18:53:15 -0400

    Some pause menu stuff
    Pause menu stuff added

--- Unity/Outpost Horizon Mobile/Assets/Scripts/JScripts/Options.cs
+++ Unity/Outpost Horizon Mobile/Assets/Scripts/JScripts/Options.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Options : MonoBehaviour
 {
+    public GameObject optmenu;
+    int c = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    public void MenuOpen()
     {
-        
+        if (optmenu.activeInHierarchy == true)
+        {
+            optmenu.SetActive(false);
+        }
+        else if (optmenu.activeInHierarchy == false)
+        {
+            optmenu.SetActive(true);
+        }
+        StartCoroutine(enableDebug());
+    }
+    public void ResetOpt()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void DevSet()
+    {
+        SceneManager.LoadScene(0);
+    }
+    IEnumerator enableDebug()
+    {
+        c++;
+        yield return new WaitForSeconds(3);
+        if (c >= 10)
+        {
+            GameManagerScript.instance.debugStart();
+            StopAllCoroutines();
+        }
     }
 }
